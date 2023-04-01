657c3bc7ae9bcb61d706cc3216b71de92f20a7d6
Christopher Spadavecchia <cspada04@gmail.com>
2023-04-01 17:34:19 -0400

    version 1.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
-import Head from 'next/head'
-import { Box, Button, Heading, useColorMode } from '@chakra-ui/react'
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
-
-
+import Head from "next/head";
+import {
+  AbsoluteCenter,
+  Box,
+  Button,
+  Heading,
+  useColorMode,
+} from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Center,
+  Flex,
+  Grid,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 const IndexPage = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <>
@@ -13,13 +28,75 @@ const IndexPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Heading as='h1' size='4xl'>College Finder</Heading>
-        <Button onClick={toggleColorMode}>
-          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-        </Button>
+        <Flex flexDirection={"row"} alignItems={"center"} mx={5} mb={70} mt={5}>
+          <Image src="/icon_img.svg" fixed height={50} width={50} />
+          <Heading as="h5" size="lg" mx={2}>
+            College Finder
+          </Heading>
+        </Flex>
+
+        <Center>
+          <Flex flexDirection="column">
+            <Box maxW="64rem">
+              <Heading size="3xl" mb={50}>
+                Welcome to College Finder!
+              </Heading>
+            </Box>
+            <Box>
+              <Text fontSize="2xl" mb={15}>
+                Don't know where to go to college? No worries!
+              </Text>
+              <Text fontSize="2xl" mb={50}>
+                Take our quiz to find out the best college for you!
+              </Text>
+            </Box>
+            <Box>
+              <Button size="lg" colorScheme="green" mt="24px">
+                Get Started Now
+              </Button>
+            </Box>
+          </Flex>
+        </Center>
+
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.bgImage}
+            src="/landing_img.jpg"
+            fixed
+            // layout="responsive"
+            height={250}
+            width={400}
+          />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.cgImage}
+            src="/campus_img.jpg"
+            fixed
+            // layout="responsive"
+            height={250}
+            width={400}
+          />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.dgImage}
+            src="/student_img.jpg"
+            fixed
+            // layout="responsive"
+            height={250}
+            width={400}
+          />
+        </div>
+
+        <Box position={"fixed"} bottom={0} left={0} p={4}>
+          <Button maxW="8rem" onClick={toggleColorMode}>
+            Toggle {colorMode === "light" ? "Dark" : "Light"}
+          </Button>
+        </Box>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
